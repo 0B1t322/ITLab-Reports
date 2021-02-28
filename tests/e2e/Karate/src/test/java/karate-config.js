e1887bb033ab18806cafd4354b0c6895bcb9034f
Maksim Makushchenko <maksalmak@gmail.com>
2021-02-28 10:52:21 +0300

    Tests for admin token

--- tests/e2e/Karate/src/test/java/karate-config.js
+++ tests/e2e/Karate/src/test/java/karate-config.js
@@ -7,9 +7,20 @@ function fn() {
     var config = {
         env: env,
         baseUrl: 'http://localhost:8080/',
-        adminAuthToken: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IjEyMyJ9.eyJhdWQiOiJpdGxhYiIsImlzcyI6Imh0dHBzOi8vZGV2LmlkZW50aXR5LnJ0dWl0bGFiLnJ1IiwiaWF0IjoxNTE2MjM5MDIyLCJleHAiOjE1MDU0Njc3NTY4NjksInN1YiI6IjMyMS1sb2wtMzIxIiwiaXRsYWIiOlsicmVwb3J0cy5hZG1pbiIsInJlcG9ydHMudXNlciJdLCJzY29wZSI6WyJyb2xlcyIsIm9wZW5pZCIsInByb2ZpbGUiLCJpdGxhYi5ldmVudHMiLCJpdGxhYi5yZXBvcnRzIl19.N-rLG1WyhtUA-FwyDtebs4GkncvKMxcEUeQlN4elkc8',
-        userAuthToken: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IjEyMyJ9.eyJhdWQiOiJpdGxhYiIsImlzcyI6Imh0dHBzOi8vZGV2LmlkZW50aXR5LnJ0dWl0bGFiLnJ1IiwiaWF0IjoxNTE2MjM5MDIyLCJleHAiOjE1MDU0Njc3NTY4NjksInN1YiI6IjMyMS1sb2wtMzIxIiwiaXRsYWIiOiJ1c2VyIiwic2NvcGUiOlsicm9sZXMiLCJvcGVuaWQiLCJwcm9maWxlIiwiaXRsYWIuZXZlbnRzIiwiaXRsYWIucmVwb3J0cyJdfQ.YhVzNIoyfkqs_fJ8qHLLP7qJ1YWdsSUe4JgRTSff-6g',
-        noITLabUserAuthToken: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IjEyMyJ9.eyJhdWQiOiJpdGxhYiIsImlzcyI6Imh0dHBzOi8vZGV2LmlkZW50aXR5LnJ0dWl0bGFiLnJ1IiwiaWF0IjoxNTE2MjM5MDIyLCJleHAiOjE1MDU0Njc3NTY4NjksInN1YiI6IjMyMS1sb2wtMzIxIiwiaXRsYWIiOiJyZXBvcnRzLnVzZXIiLCJzY29wZSI6WyJyb2xlcyIsIm9wZW5pZCIsInByb2ZpbGUiLCJpdGxhYi5ldmVudHMiLCJpdGxhYi5yZXBvcnRzIl19.-kF-Zcxc6j3PYHLGHBqBnWIt1HIEZtoePt-waVqZ7zo'
+        users: {
+            admin: {
+                id: 'user-admin',
+                accessToken: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IjEyMyJ9.eyJhdWQiOiJpdGxhYiIsImlzcyI6Imh0dHBzOi8vZGV2LmlkZW50aXR5LnJ0dWl0bGFiLnJ1IiwiaWF0IjoxNTE2MjM5MDIyLCJleHAiOjE1MDU0Njc3NTY4NjksInN1YiI6InVzZXItYWRtaW4iLCJpdGxhYiI6WyJyZXBvcnRzLmFkbWluIiwidXNlciJdLCJzY29wZSI6WyJyb2xlcyIsIm9wZW5pZCIsInByb2ZpbGUiLCJpdGxhYi5ldmVudHMiLCJpdGxhYi5yZXBvcnRzIl19.Wksaz3faLzdEkWMaAjC0gkYT3SxRuHeLrlQordlQKes'
+            },
+            plain: {
+                id: 'user-plain',
+                accessToken: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IjEyMyJ9.eyJhdWQiOiJpdGxhYiIsImlzcyI6Imh0dHBzOi8vZGV2LmlkZW50aXR5LnJ0dWl0bGFiLnJ1IiwiaWF0IjoxNTE2MjM5MDIyLCJleHAiOjE1MDU0Njc3NTY4NjksInN1YiI6InVzZXItcGxhaW4iLCJpdGxhYiI6InVzZXIiLCJzY29wZSI6WyJyb2xlcyIsIm9wZW5pZCIsInByb2ZpbGUiLCJpdGxhYi5ldmVudHMiLCJpdGxhYi5yZXBvcnRzIl19.hYPp4w6zJnL1ZQlO7TdaU7Jjvxz8hdF81uxjR0Xa6UY'
+            },
+            incorrect: {
+                id: 'user-incorrect',
+                accessToken: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IjEyMyJ9.eyJhdWQiOiJpdGxhYiIsImlzcyI6Imh0dHBzOi8vZGV2LmlkZW50aXR5LnJ0dWl0bGFiLnJ1IiwiaWF0IjoxNTE2MjM5MDIyLCJleHAiOjE1MDU0Njc3NTY4NjksInN1YiI6InVzZXItaW5jb3JyZWN0IiwiaXRsYWIiOiJyZXBvcnRzLnVzZXIiLCJzY29wZSI6WyJyb2xlcyIsIm9wZW5pZCIsInByb2ZpbGUiLCJpdGxhYi5ldmVudHMiLCJpdGxhYi5yZXBvcnRzIl19.VNq05e-GsOX1QFOxQfom0uQq3xBLYzOHEf5wuDI7pM8'
+            }
+        }
     }
     if (env == 'dev') {
         // customize
