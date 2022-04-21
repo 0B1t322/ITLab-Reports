2e82528bc91d005b25eab63bfad269eaff244875
0B1t322 <dan.demin@gmail.com>
2022-04-21 11:36:04 +0300

    add error encoder

---
+++ src/ITLabReports/transport/report/http/options/servererrorencoder/error_encoder.go
@@ -0,0 +1,47 @@
+package servererrorencoder
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/RTUITLab/ITLab-Reports/pkg/errors"
+	"github.com/RTUITLab/ITLab-Reports/service/reports"
+	"github.com/RTUITLab/ITLab-Reports/transport/middlewares"
+	"github.com/clarketm/json"
+	"github.com/sirupsen/logrus"
+)
+
+type ErrorModel struct {
+	Error string	`json:"error"`
+}
+
+func EncodeError(ctx context.Context, err error, w http.ResponseWriter) {
+	var statusCode int
+
+	switch {
+	// Forbidden errors
+	case err == middlewares.NotAdmin, err == middlewares.NotSuperAdmin:
+		statusCode = http.StatusForbidden
+	// Unauth
+	case 	err == middlewares.FailedToParseToken, err == middlewares.RoleNotFound, 
+			err == middlewares.TokenNotValid, err == middlewares.TokenExpired:
+		statusCode = http.StatusUnauthorized
+	// BadRequest 
+	case err == reports.ErrReportIDNotValid, errors.Is(err, reports.ErrValidationError):
+		statusCode = http.StatusBadRequest
+	// NotFound
+	case err == reports.ErrReportNotFound:
+		statusCode = http.StatusNotFound
+	default:
+		statusCode = http.StatusInternalServerError
+		logrus.WithFields(
+			logrus.Fields{
+				"from": "reports",
+				"err": err,
+			},
+		).Error("")
+	}
+
+	w.WriteHeader(statusCode)
+	json.NewEncoder(w).Encode(ErrorModel{Error: err.Error()})
+}
